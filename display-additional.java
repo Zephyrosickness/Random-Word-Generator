c75e1eb3ac09a38a4e57184e96db95c2ace966db
DeadlyFern524 <samisgr84all@yahoo.com>
2024-07-17 23:42:18 -0500

    has 'ing' and 'uncommon letters' modifiers

---
+++ display-additional.java
@@ -0,0 +1,185 @@
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+
+//contains most graphical data
+//welcome to hell
+
+public class display extends JFrame {
+    /*ppl keep saying static vars are bad and i mean thats probably true because of something something nerd stuff idk about but um im bad at coding and i keep
+    having problems idk how to fix without static vars idek why theyre bad but um maybe one day ill be good at coding and it will be fixed*/
+
+    public static int frameWid = 500;
+    public static int frameHeight = 400;
+    public static int margins = 10; // i asked santa for some variables and he gave me these
+
+    public static void main(String[] args) {
+
+        //changes l&f to windows classic because im a basic bitch like that
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows Classic".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            //error handler for l&f in case something doesnt work
+            System.out.println("error with look and feel");
+        }
+
+        // create a window
+        JFrame frame = new JFrame("word generator");
+        frame.setSize(frameWid, frameHeight);
+
+        //word display
+
+        //make new panel this will have the words in it
+        JPanel wordPanel = new JPanel();
+        wordPanel.setLayout(new BoxLayout(wordPanel, BoxLayout.Y_AXIS));
+
+        // Create a JScrollPane and add the content panel to it
+        JScrollPane wordDisplay = new JScrollPane(wordPanel);
+        Integer displayWid = (frameWid / 2) - (margins * 2);
+        Integer displayHeight = frameHeight - (margins * 2);
+        wordDisplay.setBounds((frameWid / 2) - margins, margins, displayWid, displayHeight);
+
+        // Add the scroll pane to the frame
+        frame.add(wordDisplay);
+
+        //main panel to hold components
+        JPanel panel = new JPanel();
+
+        //adds panel
+        frame.add(panel);
+        panel.setBounds(0, 0, 200, 400);
+        frame.setResizable(false);
+        songInfoComponents(panel, wordPanel);
+        frame.setVisible(true);
+    }
+
+    //panel to hold components
+    public static void songInfoComponents(JPanel panel, JPanel wordPanel) {
+
+        // set the layout manager to null for absolute positioning
+        panel.setLayout(null);
+
+
+        //filter option labels
+
+        int queryHeight = 25;
+        int halfOne = Math.round((((frameWid / 2) - (margins * 2)) / 3 ));
+        int halfTwo = (halfOne * 2);
+        // santa please i have enough variables i do not want them for my birthday also oh geez
+
+        //System.out.println("Test 1: " + halfOne);
+        //System.out.println("Test 2: " + halfTwo);
+        //System.out.println("Test 3: " + halfThree);
+        //System.out.println("Test 4: " + frameWid);
+        //System.out.println("Test 5: " + margins); // the hoarde
+
+        //label for word length
+        JLabel lengthLabel = new JLabel("Word Length");
+        lengthLabel.setBounds(margins, margins, halfOne, queryHeight);
+        panel.add(lengthLabel);
+
+        //label for consonant
+        JLabel vowelLabel = new JLabel("Vowel Distance");
+        vowelLabel.setBounds(margins, (margins + queryHeight), halfOne, queryHeight);
+        panel.add(vowelLabel);
+
+        //label for first letter
+        JLabel startLabel = new JLabel("First Letter");
+        startLabel.setBounds(margins, (margins + (queryHeight*2)), halfOne, queryHeight);
+        panel.add(startLabel);
+
+        //DROPDOWNS
+
+
+        //array vars (dropdown options)
+
+
+        //for op dropdowns
+        String[] operators = {"=","<", "Any"};
+
+        String[] letters = {"Any Letter","Any Consonant","Any Vowel", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+
+        //filter dropdowns
+
+        int moveForward = (margins + halfOne);
+        int moveForward2 = (margins + halfTwo);
+
+        //dropdown for operator on word length
+        final JComboBox<String> lengthOperator = new JComboBox<>(operators);
+        lengthOperator.setBounds(moveForward, margins,  halfOne, queryHeight);
+        panel.add(lengthOperator);
+
+        //dropdown for operator on vowel space
+        final JComboBox<String> vowelOperator = new JComboBox<>(operators);
+        vowelOperator.setBounds(moveForward, (margins + queryHeight),  halfOne, queryHeight);
+        panel.add(vowelOperator);
+
+        //dropdown for first letter
+        final JComboBox<String> startingLetter = new JComboBox<>(letters);
+        startingLetter.setBounds(moveForward, (margins + (queryHeight*2)),  halfTwo, queryHeight);
+        panel.add(startingLetter);
+
+        //dropdown for uncommon letters boolean
+        final JCheckBox uncommonCheck = new JCheckBox("Uncommon Letters?");
+        uncommonCheck.setBounds(moveForward, (margins + (queryHeight*3)),  halfTwo, queryHeight);
+        panel.add(uncommonCheck);
+
+        //dropdown for adding "ing" boolean
+        final JCheckBox ingCheck = new JCheckBox("Append 'ING'?");
+        ingCheck.setBounds(moveForward, (margins + (queryHeight*4)),  halfTwo, queryHeight);
+        panel.add(ingCheck);
+
+        // NUMBERS sorry i removed the old comment here so im making my own
+
+        //field for word length
+        JTextField lengthField = new JTextField("0");
+        lengthField.setBounds(moveForward2, margins, halfOne, queryHeight);
+        panel.add(lengthField);
+
+        //field for vowel
+        JTextField vowelField = new JTextField("0");
+        vowelField.setBounds(moveForward2, (margins + queryHeight), halfOne, queryHeight);
+        panel.add(vowelField);
+
+
+        ///BUTTONS
+
+
+        //button to run word calcs
+        JButton run = new JButton("Generate");
+        run.setFont(new Font("Arial", Font.PLAIN, 20));
+        run.setBackground(new Color(255,255,255));
+        //run.setForeground(new Color(0,0,0));
+        run.setBorder(BorderFactory.createDashedBorder(Color.black, 2, 3));
+
+        int newWidth = (frameWid / 2) - (margins * 3);
+
+        run.setBounds(margins, frameHeight - 145, newWidth, 100);
+        panel.add(run);
+
+
+        //ACTIONS
+
+        //calculates words (run button)
+        run.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JLabel result = new JLabel(gen.generate(Integer.parseInt((String)lengthField.getText()), Integer.parseInt((String)vowelField.getText()), (String)startingLetter.getSelectedItem(), (String)lengthOperator.getSelectedItem(), (String)vowelOperator.getSelectedItem(), (Boolean)uncommonCheck.isSelected(), (Boolean)ingCheck.isSelected()));
+                // santa why is your accursed result 331 characters long
+                wordPanel.add(result);   
+                wordPanel.repaint();
+                wordPanel.revalidate();  
+            }
+        });
+
+    }
+}
---
+++ gen-additional.java
@@ -0,0 +1,110 @@
+import java.util.Random;
+
+public class gen {
+    public static String generate(int lengthInit, int vowelInit, String startLetter, String lengthOp, String vowelOp, Boolean uncommonLet, Boolean addIng){
+        Random rand = new Random();
+        String[] vowels = {"a","e","i","o","u"};
+        String[] allConsonants = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"};
+        String[] reducedConsonants = {"b","c","d","f","g","h","j","k","l","m","n","p","r","s","t"};
+        String[] all = {"A","E","I","O","U","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"};
+        String[] reducedAll = {"A","E","I","O","U","B","C","D","F","G","H","J","K","L","M","N","P","R","S","T"};
+        String finalWord = "";
+        int length = 1;
+        int vowelDistance = 1;
+        
+
+        if(startLetter=="Any Letter"){
+            if(uncommonLet){
+                finalWord = randomize(all);
+                }else{
+                finalWord = randomize(reducedAll);
+                }
+        }else if(startLetter == "Any Consonant"){
+            if(uncommonLet){
+            finalWord = randomize(allConsonants);
+            }else{
+            finalWord = randomize(reducedConsonants);
+            }
+        }else if(startLetter == "Any Vowel"){
+            finalWord = randomize(vowels);
+        }else{
+            finalWord = startLetter;
+        }
+
+
+        switch(vowelOp){
+            case "Any":
+                vowelDistance = rand.nextInt(4);
+                break;
+            case "<":
+                vowelDistance = rand.nextInt(vowelInit);
+                break;
+            case "=":
+                vowelDistance = vowelInit;
+                break;
+            default:
+                System.out.println("something fucked up with the vowelop lil bro. vowelop: "+vowelOp);
+        }
+        if(vowelDistance == 0){
+            vowelDistance = 1;
+        }
+
+        switch(lengthOp){
+            case "Any":
+                length = rand.nextInt(13);
+                break;
+            case "<":
+                length = rand.nextInt(lengthInit);
+                break;
+            case "=":
+                length = lengthInit;
+                break;
+            default:
+                System.out.println("something fucked up with the lengthop lil bro. lengthop: "+lengthOp);
+        }
+
+        //System.out.println(length);
+
+
+        //inserts random letters until the length is met
+        for(@SuppressWarnings("unused")
+        int i = 0; finalWord.length()<length;){
+            for(int j = 1; j<=vowelDistance&&finalWord.length()<length; j++){
+
+                if(uncommonLet){
+                finalWord = finalWord+randomize(allConsonants);
+                }else{
+                    finalWord = finalWord+randomize(reducedConsonants);
+                }
+
+                //inserts a vowel after voweldistance is met (for example, if voweldistance is 2, only add a vowel every 2 letters)
+                if(j==vowelDistance&&finalWord.length()<length){
+                    finalWord = finalWord+randomize(vowels);
+                }
+            }
+        }
+        if(addIng){
+            if (finalWord.endsWith("e")) {
+                StringBuffer stringBuffer = new StringBuffer(finalWord);
+                if (stringBuffer.length() > 0) {
+                    stringBuffer.deleteCharAt(stringBuffer.length() - 1);
+                }
+                finalWord = (stringBuffer.toString());
+            }
+            return finalWord+"ing";
+            }else{
+            return finalWord;
+            }
+    }
+
+    public static String randomize(String[] array){
+
+        Random rand = new Random();
+        int resultIndex = rand.nextInt(array.length);
+        if(resultIndex==0){
+            resultIndex = 1;
+        }
+        return array[resultIndex];
+
+    }
+}
